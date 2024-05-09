b7547415acaa8eb09234d085fd3e1a0259682c40
Federico Rossendy <fede07@users.noreply.github.com>
2024-05-09 20:27:32 -0300

    Inicio Sistema Automotor

---
+++ src/main/java/grupo2/SistemaAutomotor/SistemaAutomotorApplication.java
@@ -0,0 +1,13 @@
+package grupo2.SistemaAutomotor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAutomotorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAutomotorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grupo2/SistemaAutomotor/SistemaAutomotorApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo2.SistemaAutomotor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAutomotorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
