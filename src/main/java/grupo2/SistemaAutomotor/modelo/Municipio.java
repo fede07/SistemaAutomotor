2e3a8e3fc2b9e6bd15d1d5c5fda70c26e2830cf6
Jorge Lopez <jorgelopez126@hotmail.com>
2024-05-10 11:59:26 -0300

    Municipio

---
+++ src/main/java/grupo2/SistemaAutomotor/modelo/Municipio.java
@@ -0,0 +1,24 @@
+package grupo2.SistemaAutomotor.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
+
+public class Municipio {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private String Descripcion;
+    private String Localidad;
+}
