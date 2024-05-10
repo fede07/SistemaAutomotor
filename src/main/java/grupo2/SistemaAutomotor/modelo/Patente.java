67f02777f33a43c57cd17cfe665f86662177f880
Jorge Lopez <jorgelopez126@hotmail.com>
2024-05-10 12:20:21 -0300

    Patente FK

---
+++ src/main/java/grupo2/SistemaAutomotor/modelo/Patente.java
@@ -0,0 +1,29 @@
+package grupo2.SistemaAutomotor.modelo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+
+public class Patente {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int IdPatente;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "Automotor", foreignKey = @ForeignKey(name = "dominio"))
+    private Automotor dominio;
+}
