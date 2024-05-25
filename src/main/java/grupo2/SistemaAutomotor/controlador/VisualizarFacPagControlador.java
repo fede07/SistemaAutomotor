a07653c8dc0165abe4d0a879ecc8ba5f81ad3348
Jorge Lopez <jorgelopez126@hotmail.com>
2024-05-25 15:56:27 -0300

    Visualizar Facturas Pagadas

---
+++ src/main/java/grupo2/SistemaAutomotor/controlador/VisualizarFacPagControlador.java
@@ -0,0 +1,30 @@
+package grupo2.SistemaAutomotor.controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Setter
+@Component
+public class VisualizarFacPagControlador implements Initializable {
+
+    private Scene mainScene;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void abrirMainScene(ActionEvent actionEvent){
+        Stage mainStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        mainStage.setScene(mainScene);
+    }
+
+}
