9890e3cbfae4bad7c29ff77a2306ab4f5da3ea15
Federico Nicolas Loiero <fede_loi95@hotmail.com>
2024-05-26 16:04:16 -0300

    agrego recaudacion municipio

---
+++ src/main/java/grupo2/SistemaAutomotor/controlador/RecadacionMunicipioControlador.java
@@ -0,0 +1,13 @@
+package grupo2.SistemaAutomotor.controlador;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RecadacionMunicipioControlador implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
