a230635c6dec1d32d06fc3f875dec78d0927d6af
Jorge Lopez <jorgelopez126@hotmail.com>
2024-05-26 16:07:01 -0300

    Agrego Lista y boletas

--- src/main/java/grupo2/SistemaAutomotor/controlador/VisualizarFacPagControlador.java
+++ src/main/java/grupo2/SistemaAutomotor/controlador/VisualizarFacPagControlador.java
@@ -1,22 +1,62 @@
 package grupo2.SistemaAutomotor.controlador;
 
+import grupo2.SistemaAutomotor.modelo.Boleta;
+import grupo2.SistemaAutomotor.servicio.boleta.BoletaServicio;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
-
+import java.math.BigDecimal;
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-@Setter
 @Component
 public class VisualizarFacPagControlador implements Initializable {
 
+    @Setter
     private Scene mainScene;
+
+    private BoletaServicio boletaServicio;
+
+    @FXML
+    private TableView<Boleta> boletaTabla = new TableView<>();
+
+    @FXML
+    private TableColumn<Boleta, Integer> cuotaColumna;
+
+    @FXML
+    private TableColumn<Boleta, BigDecimal> importeColumna;
+
+    @FXML
+    private TableColumn<Boleta, Date> fechadevenColumna;
+
+    private final ObservableList<Boleta> boletaList = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        boletaTabla.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        configurarColumnas();
+        listarCuotas();
+    }
 
+    private void configurarColumnas(){
+        cuotaColumna.setCellValueFactory(new PropertyValueFactory<>("cuota"));
+        importeColumna.setCellValueFactory(new PropertyValueFactory<>("importe"));
+        fechadevenColumna.setCellValueFactory(new PropertyValueFactory<>("fechaVencimiento"));
+    }
+
+    private void listarCuotas(){
+        boletaList.clear();
+        boletaList.addAll(boletaServicio.listarBoletas());
+        boletaTabla.setItems(boletaList);
     }
 
 }
