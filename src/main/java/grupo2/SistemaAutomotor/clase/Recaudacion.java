0a94cbeeb5c1ca6f2592c35d1b2c900b3034013a
Federico Rossendy <fede07@users.noreply.github.com>
2024-06-28 17:52:55 -0300

    Toques Finales

--- src/main/java/grupo2/SistemaAutomotor/clase/Recaudacion.java
+++ src/main/java/grupo2/SistemaAutomotor/clase/Recaudacion.java
@@ -2,9 +2,15 @@ package grupo2.SistemaAutomotor.clase;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 @Data
 public class Recaudacion {
     private String nombreMunicipio;
-    private float total;
+    private BigDecimal total;
 
+    public void setTotal(BigDecimal total) {
+        this.total = total.setScale(2, RoundingMode.HALF_UP);
+    }
 }
