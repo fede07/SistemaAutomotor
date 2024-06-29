9d3177b5064ed399274a915d2872980fe3599ce5
Federico Nicolas Loiero <fede_loi95@hotmail.com>
2024-06-29 11:59:51 -0300

    Revision

--- src/main/java/grupo2/SistemaAutomotor/controlador/ConsultarControlador.java
+++ src/main/java/grupo2/SistemaAutomotor/controlador/ConsultarControlador.java
@@ -1,4 +1,34 @@
 package grupo2.SistemaAutomotor.controlador;
 
-public class ConsultarControlador {
+import grupo2.SistemaAutomotor.modelo.Automotor;
+import grupo2.SistemaAutomotor.modelo.Titular;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Component
+public class ConsultarControlador implements Initializable {
+    public TextField dominioTextField;
+    public Button buscarAutomotorButton;
+    public TableColumn<Automotor, String> dominioColumna;
+    public TableColumn<Automotor, Integer> titularColumna;
+    public TableColumn<Automotor, String> marcaColumna;
+    public TableColumn<Automotor, String> modeloColumna;
+    public TableColumn<Automotor, String> municipioColumna;
+    public TableColumn<Automotor, Integer> anioColumna;
+    public TextField dniTextField;
+    public Button buscarDniButton;
+    public TableColumn<Titular, Integer> dniTitularColumna;
+    public TableColumn<Titular, String>nombreTitularColumna;
+    public TableColumn<Titular, String> apellidoTitularColumna;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
